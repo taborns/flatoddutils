1ab9002532e2e83165ce92adb3012173400e4bee
samuel <samuelhadush@gmail.com>
2024-11-25 18:10:02 +0300

    feat: class 110 added

--- slipcomputer.js
+++ slipcomputer.js
@@ -5742,6 +5742,54 @@ class MBNS_MW350K_SlpSz35_3_40MCH extends MBNS_MW300K_SlpSz35_3_20MCH{
     }
 }
 
+class MultiBonus10_No_Tax extends MultiBonus10 {
+
+    get_configurations(configuration_name) {
+
+        let all_configurations = this.get_all_configurations()
+        all_configurations["BET_SLIP_BONUS"] = true
+        all_configurations["MAX_WIN"] = 500000
+        all_configurations["MAX_BONUS"] = 52400
+        all_configurations["SLIP_SIZE"] = 30
+        all_configurations["MIN_BONUS_ODD"] = 1.4
+        all_configurations["TAX_TYPE"] = configurations.TAX_TYPE_NONE
+
+        return all_configurations[configuration_name]
+    }
+    get_min_bonus_eligble_match_count() {
+        return 3
+    }
+    get_max_bonus_eligble_match_count() {
+        return 20
+    }
+
+    get_percentages(match_count) {
+        let count = match_count > 20 ? 20 : match_count
+        return {
+            1: 0.00,
+            1: 0.00,
+            3: 0.03,
+            4: 0.04,
+            5: 0.05,
+            6: 0.06,
+            7: 0.1,
+            8: 0.15,
+            9: 0.2,
+            10: 0.25,
+            11: 0.3,
+            12: 0.35,
+            13: 0.4,
+            14: 0.45,
+            15: 0.5,
+            16: 0.55,
+            17: 0.6,
+            18: 0.65,
+            19: 0.7,
+            20: 0.75,
+        }[count]
+    }
+}
+
 export default {
 
     SlipComp: SlipComp,
@@ -5853,5 +5901,6 @@ export default {
     MBNS_MW300K_SlpSz35_3_20MCH: MBNS_MW300K_SlpSz35_3_20MCH,
     MBNS_MW350K_SlpSz30_3_18MCH: MBNS_MW350K_SlpSz30_3_18MCH,
     MBNS_MW350K_55SLP_6_38MCH: MBNS_MW350K_55SLP_6_38MCH,
-    MBNS_MW350K_SlpSz35_3_40MCH: MBNS_MW350K_SlpSz35_3_40MCH
+    MBNS_MW350K_SlpSz35_3_40MCH: MBNS_MW350K_SlpSz35_3_40MCH,
+    MultiBonus10_No_Tax: MultiBonus10_No_Tax
 }
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import slipcomp from './index.js'
 const bonus_calculator = slipcomp.getSlipComputer(
-    'MBNS_MW350K_SlpSz35_3_40MCH',
+    'MultiBonus10_No_Tax',
     10,      // Example bet amount
     800.0,      // Example total odds
     25,      // Example match count
